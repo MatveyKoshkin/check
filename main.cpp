a97fec824cfebc6460a2327458026bde638012e9
MatveyKoshkin <113891864+MatveyKoshkin@users.noreply.github.com>
2022-10-20 23:36:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+int main()
+{
+  int arr[] = { 13, 41, 34, 15, 19, 16, 20, 65, 18, 17, 90 };
+  int lenght = sizeof(arr) / sizeof(int);
+  for (int i = 0; i < lenght; i ++)
+  {
+    printf("%d\n, check(arr[i]));
+  {
+  return 0;
+}             
