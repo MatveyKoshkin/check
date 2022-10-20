729dc8b40c5d042541c3e110379700945c21f01a
MatveyKoshkin <113891864+MatveyKoshkin@users.noreply.github.com>
2022-10-20 23:36:48 +0300

    Create check.cpp

---
+++ check.cpp
@@ -0,0 +1,12 @@
+int check(int num)
+{
+	switch (num)
+	{
+	case 15: {return num; break; }
+	case 16: {return num; break; }
+	case 17: {return num; break; }
+	case 18: {return num; break; }
+	case 19: {return num; break; }
+	default: return 0;
+	}
+}
